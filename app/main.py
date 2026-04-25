7fe28d0035cc70ca513571d05207803e432d3e3b
Felipe Duque <felipe@duque.dev>
2026-04-25 08:36:55 -0300

    cors fix

--- app/main.py
+++ app/main.py
@@ -60,6 +60,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:8080",
         "https://k0s.app",
+        "https://www.k0s.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
